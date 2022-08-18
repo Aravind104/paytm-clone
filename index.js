77d2b0a71ad9b955f539b4fe8a0148bbd275bbb3
Aravind104 <ontagodiaravind@gmail.com>
2022-08-18 22:05:00 +0530

    Add business-services

--- index.js
+++
@@ -1,6 +0,0 @@
-const burger=document.querySelector('#ham-burger');
-const content=document.querySelector('#menu-items');
-
-burger.addEventListener('click',()=>{
-   content.classList.toggle('hidden')
-})
